a98be69dd644703d8da7401bf94b9957a88e579c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 02:45:10 +0300

    Added authentication for the project

--- DotnetAuthYetAnother.Api/Program.cs
+++ DotnetAuthYetAnother.Api/Program.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using DotnetAuthYetAnother.Api.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,28 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig").GetSection("Secret"));
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt =>
+{
+    var key = builder.Configuration.GetSection("JwtConfig").GetSection("Secret").Value;
+
+    jwt.SaveToken = true;
+
+    jwt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!)),
+        ValidateIssuer = false, //TODO: Change to true on deployment
+        ValidateAudience = false, //TODO Change to true on deployment
+        RequireExpirationTime = false, //TODO Change to true on deployment
+        ValidateLifetime = true
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
