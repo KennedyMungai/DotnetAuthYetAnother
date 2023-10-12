c8c877e5ae415d702d4a1392d9659fbb63e7cb09
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 14:28:43 +0300

    Wrote mostt of the Register method

--- DotnetAuthYetAnother.Api/Controllers/AuthenticationController.cs
+++ DotnetAuthYetAnother.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
 using DotnetAuthYetAnother.Api.Configuration;
+using DotnetAuthYetAnother.Api.Models;
+using DotnetAuthYetAnother.Api.Models.Dtos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,5 +17,48 @@ public class AuthenticationController : ControllerBase
     {
         _userManager = userManager;
         _config = config;
+    }
+
+    [HttpPost("{Register}")]
+    public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto requestDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return await Task.FromResult(BadRequest(ModelState));
+        }
+
+        var userExists = await _userManager.FindByEmailAsync(requestDto.Email);
+
+        if (userExists is not null)
+        {
+            return await Task.FromResult(BadRequest(new AuthResults()
+            {
+                Result = false,
+                Errors = new List<string>()
+                {
+                    "The Email already exists"
+                }
+            }));
+        }
+
+        var newUser = new IdentityUser()
+        {
+            Email = requestDto.Email,
+            UserName = requestDto.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(requestDto.Password)
+        };
+
+        var isCreated = await _userManager.CreateAsync(newUser);
+
+        if (!isCreated.Succeeded)
+        {
+            return await Task.FromResult(BadRequest(new AuthResults()
+            {
+                Errors = isCreated.Errors.Select(x => x.Description).ToList(),
+                Result = false
+            }));
+        }
+
+        // TODO: Generate the user tokens
     }
 }
