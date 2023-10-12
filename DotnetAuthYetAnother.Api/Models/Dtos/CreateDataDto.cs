2aa5bf56c67411fbe02311d2e3cf33a34d279a4e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 04:23:45 +0300

    Added the id field

--- DotnetAuthYetAnother.Api/Models/Dtos/CreateDataDto.cs
+++ DotnetAuthYetAnother.Api/Models/Dtos/CreateDataDto.cs
@@ -3,6 +3,7 @@ namespace DotnetAuthYetAnother.Api.Models.Dtos;
 
 public class CreateDataDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Country { get; set; } = string.Empty;
     public string TeamPrinciple { get; set; } = string.Empty;
