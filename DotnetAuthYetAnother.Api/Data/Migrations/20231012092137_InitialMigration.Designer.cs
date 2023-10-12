bd6c57384ff76d9be0b847f4e146a17792d9bfed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 12:23:06 +0300

    A fresh set of migrations

--- DotnetAuthYetAnother.Api/Data/Migrations/20231012092137_InitialMigration.Designer.cs
+++ DotnetAuthYetAnother.Api/Data/Migrations/20231012092137_InitialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DotnetAuthYetAnother.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotnetAuthYetAnother.Api.Data.Migrations
 {
     [DbContext(typeof(FormulaOneDbContext))]
-    partial class FormulaOneDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231012092137_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
