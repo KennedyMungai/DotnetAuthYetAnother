8f9e9a68c5790116b197ae3c21e883d2f9766785
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 15:23:18 +0300

    Added data annotations to the model fields

--- DotnetAuthYetAnother.Api/Models/Dtos/UserLoginRequestDto.cs
+++ DotnetAuthYetAnother.Api/Models/Dtos/UserLoginRequestDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DotnetAuthYetAnother.Api.Models.Dtos;
 
 
 public class UserLoginRequestDto
 {
+    [Required]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
