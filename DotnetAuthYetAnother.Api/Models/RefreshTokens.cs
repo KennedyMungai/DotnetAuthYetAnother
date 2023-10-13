8020d6c18b3858db0b2a1b69a3c6d0db9548e6b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 12:36:37 +0300

    Updated the JwtToken field name to Token

--- DotnetAuthYetAnother.Api/Models/RefreshTokens.cs
+++ DotnetAuthYetAnother.Api/Models/RefreshTokens.cs
@@ -5,7 +5,7 @@ public class RefreshTokens
 {
     public int Id { get; set; }
     public string UserId { get; set; } = string.Empty;
-    public string JwtToken { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
     public string JwtId { get; set; } = string.Empty;
     public bool IsUsed { get; set; }
     public bool IsRevoked { get; set; }
