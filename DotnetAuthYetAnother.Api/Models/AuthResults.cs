ae4bed477000efba220d1f6f6e5f0c2d866a5e7d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 12:06:01 +0300

    Added the refresh token field to the model

--- DotnetAuthYetAnother.Api/Models/AuthResults.cs
+++ DotnetAuthYetAnother.Api/Models/AuthResults.cs
@@ -4,6 +4,7 @@ namespace DotnetAuthYetAnother.Api.Models;
 public class AuthResults
 {
     public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
     public bool Result { get; set; } = false;
     public List<string> Errors { get; set; } = new();
 }
