2a36acb41cce711a292011163c541274fff8b7a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 12:00:05 +0300

    Added the ExpiryTimeFrame field to the JwtConfig model

--- DotnetAuthYetAnother.Api/Configuration/JwtConfig.cs
+++ DotnetAuthYetAnother.Api/Configuration/JwtConfig.cs
@@ -4,4 +4,5 @@ namespace DotnetAuthYetAnother.Api.Configuration;
 public class JwtConfig
 {
     public string Secret { get; set; } = string.Empty;
+    public TimeSpan ExpiryTimeFrame { get; set; }
 }
